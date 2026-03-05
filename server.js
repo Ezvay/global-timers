2cb35e5059f745e2f7e9d68ff8b7cb43acef3003
Ezvay <kawulok.darek8@gmail.com>
2026-03-05 22:25:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,103 +1,98 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-let timers = {};
+const timers = {}
 
-function initTimers(){
+/* tworzymy timery */
+
+function createTimers(prefix){
 
 for(let i=1;i<=8;i++){
 
-timers["KG_CH"+i]={start:null,paused:0}
-timers["LM_CH"+i]={start:null,paused:0}
+let id = prefix + "_CH" + i
 
+timers[id] = {
+time:0,
+running:false,
+lastStart:0
 }
-
-}
-
-initTimers()
-
-function getTime(t){
-
-if(t.start){
-
-return Math.floor((Date.now()-t.start)/1000)
-
-}else{
-
-return t.paused
 
 }
 
 }
 
-setInterval(()=>{
+createTimers("LM")
+createTimers("KG")
 
-let output={}
-
-for(let id in timers){
-
-output[id]=getTime(timers[id])
-
-}
-
-io.emit("update",output)
-
-},1000)
-
-
+/* socket */
 
 io.on("connection",(socket)=>{
 
-let output={}
+socket.emit("update",timers)
 
-for(let id in timers){
-
-output[id]=getTime(timers[id])
-
-}
-
-socket.emit("update",output)
-
+/* start */
 
 socket.on("start",(id)=>{
 
-let t=timers[id]
+let t = timers[id]
 
-if(!t.start){
+if(!t.running){
 
-t.start=Date.now()-t.paused*1000
+t.running = true
+t.lastStart = Date.now()
 
 }
 
+io.emit("update",timers)
+
 })
+
+/* stop */
 
 socket.on("stop",(id)=>{
 
-let t=timers[id]
+let t = timers[id]
 
-if(t.start){
+if(t.running){
 
-t.paused=Math.floor((Date.now()-t.start)/1000)
-
-t.start=null
+t.time += Math.floor((Date.now()-t.lastStart)/1000)
+t.running = false
 
 }
 
+io.emit("update",timers)
+
 })
+
+/* reset */
 
 socket.on("reset",(id)=>{
 
-timers[id]={start:null,paused:0}
+timers[id] = {
+time:0,
+running:false,
+lastStart:0
+}
+
+io.emit("update",timers)
 
 })
 
 })
 
-server.listen(process.env.PORT || 3000)
+/* port */
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+
+console.log("Server running on port "+PORT)
+
+})
