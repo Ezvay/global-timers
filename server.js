db8d994c0321c206763d7a87a45e4037af71e99c
Ezvay <kawulok.darek8@gmail.com>
2026-03-05 11:48:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,40 +10,94 @@ app.use(express.static("public"));
 
 let timers = {};
 
-for (let i = 1; i <= 8; i++) {
- timers["CH"+i] = { time:0, running:false };
+function initTimers(){
+
+for(let i=1;i<=8;i++){
+
+timers["KG_CH"+i]={start:null,paused:0}
+timers["LM_CH"+i]={start:null,paused:0}
+
+}
+
+}
+
+initTimers()
+
+function getTime(t){
+
+if(t.start){
+
+return Math.floor((Date.now()-t.start)/1000)
+
+}else{
+
+return t.paused
+
+}
+
 }
 
 setInterval(()=>{
 
- for(let id in timers){
-  if(timers[id].running){
-   timers[id].time++;
-  }
- }
+let output={}
 
- io.emit("update", timers);
+for(let id in timers){
 
-},1000);
+output[id]=getTime(timers[id])
+
+}
+
+io.emit("update",output)
+
+},1000)
+
 
 
 io.on("connection",(socket)=>{
 
- socket.emit("update", timers);
+let output={}
 
- socket.on("start",(id)=>{
-  timers[id].running = true;
- });
+for(let id in timers){
 
- socket.on("stop",(id)=>{
-  timers[id].running = false;
- });
+output[id]=getTime(timers[id])
 
- socket.on("reset",(id)=>{
-  timers[id].running = false;
-  timers[id].time = 0;
- });
+}
 
-});
+socket.emit("update",output)
 
-server.listen(process.env.PORT || 3000);
+
+socket.on("start",(id)=>{
+
+let t=timers[id]
+
+if(!t.start){
+
+t.start=Date.now()-t.paused*1000
+
+}
+
+})
+
+socket.on("stop",(id)=>{
+
+let t=timers[id]
+
+if(t.start){
+
+t.paused=Math.floor((Date.now()-t.start)/1000)
+
+t.start=null
+
+}
+
+})
+
+socket.on("reset",(id)=>{
+
+timers[id]={start:null,paused:0}
+
+})
+
+})
+
+server.listen(process.env.PORT || 3000)
